9c96de5484d2e7da8221e5b29dc1f1edb0be3745
David King <david.king550wil@gmail.com>
2021-08-18 10:47:22 -0400

    first commit

---
+++ PirateRPS/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateRPS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Writeline($"You won that round... your health is at {variable} and the enemy is at {enemyvariable}");
+            // keep track of win total
+
+            Random random = new Random();
+            bool playAgain = true;
+            string player;
+            string computer;
+            int win;
+            int loss;
+            
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+
+                while (player != "laser" && player != "netting" && player != "cannon") 
+                {
+                    Console.WriteLine("ARR, Welcome to Robot pirate Island! it be PIRATES vs ROBOTS, Choose your weapon.\n" +
+                            "LASER, CANNON, NETTING.");
+                        player = Console.ReadLine();
+                        player = player.ToLower();
+                }
+                
+
+                if (player == computer)
+                {
+                    Console.WriteLine("ARR, it be a draw!");
+                }
+            }
+        }
+    }
+}
