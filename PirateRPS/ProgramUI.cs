40ffd5fcfcf19f11a61900ae4b7060ec2b9cec82
David King <david.king550wil@gmail.com>
2021-08-18 13:22:09 -0400

    implementing game logic

--- PirateRPS/ProgramUI.cs
+++ PirateRPS/ProgramUI.cs
@@ -12,37 +12,56 @@ namespace PirateRPS
         {
             // Console.Writeline($"You won that round... your health is at {variable} and the enemy is at {enemyvariable}");
                 // keep track of win total
+                // current issues: computer stuck on one option
+                //computer ending the game before the player says they no longer want to play
+
 
                 Random random = new Random();
-                bool playAgain = true;
-                string player;
-                string computer;
+                bool keepPlaying = true;
+                string keepGoing;
+                string pirate;
+                string robot;
                 int win = 0;
                 int loss = 0;
 
-            while (playAgain)
+            while (keepPlaying != false)
             {
-                player = "";
-                computer = "";
+                keepGoing = "";
+                pirate = "";
+                robot = "";
 
-                while (player != "laser" && player != "netting" && player != "cannon")
+                while (pirate != "laser" && pirate != "netting" && pirate != "cannon")
                 {
                     Console.WriteLine("ARR, Welcome to Robot Pirate Island! it be PIRATES vs ROBOTS, Choose your weapon.\n" +
                             "LASER, CANNON, NETTING.");
-                    player = Console.ReadLine();
-                    player = player.ToLower();
+                    pirate = Console.ReadLine();
+                    pirate = pirate.ToLower();
                 }
 
-                switch (player)
+                int robotWeaponChoice = random.Next(0, 3);
+                switch (robotWeaponChoice)
                 {
-                    case "paper":
-                        if  (computer == "scissors")
+                    case 0:
+                        robot = "cannon";
+                        return;
+                    case 1:
+                        robot = "laser";
+                        return;
+                    case 2:
+                        robot = "netting";
+                        break;
+                }
+
+                switch (pirate)
+                {
+                    case "netting":
+                        if  (robot == "laser")
                         {
                             Console.WriteLine("Arrgh matey, ya lost... git gud");
                             loss++;
                         Console.WriteLine($"Your record stands at {win} and {loss}");
                         }
-                        else if (computer == "rock")
+                        else if (robot == "cannon")
                         {
                             Console.WriteLine("Congrats me Friend.... ya won one");
                                 win++;
@@ -54,14 +73,14 @@ namespace PirateRPS
                         
                         }
                         break;
-                    case "rock":
-                        if (computer == "paper")
+                    case "cannon":
+                        if (robot == "netting")
                         {
                             Console.WriteLine("Arrgh matey, ya lost... git gud");
                             loss++;
                         Console.WriteLine($"Your record stands at {win} and {loss}");
                         }
-                        else if (computer == "scissors")
+                        else if (robot == "laser")
                         {
                             Console.WriteLine("Congrats me Friend.... ya won one");
                                 win++;
@@ -73,14 +92,14 @@ namespace PirateRPS
                         }
                         break;
 
-                    case "scissors":
-                        if (computer == "rock")
+                    case "laser":
+                        if (robot == "cannon")
                         {
                             Console.WriteLine("Arrgh matey, ya lost... git gud");
                             loss++;
                         Console.WriteLine($"Your record stands at {win} and {loss}");
                         }
-                        else if (computer == "paper")
+                        else if (robot == "netting")
                         {
                             Console.WriteLine("Congrats me Friend.... ya won one");
                                 win++;
@@ -93,8 +112,22 @@ namespace PirateRPS
                         break;
                 }
 
+                Console.WriteLine(" Matey, would you like to have another go at it? Enter Yes or No");
+                keepGoing = Console.ReadLine();
+                keepGoing = keepGoing.ToLower();
 
+                if(keepGoing == "yes")
+                {
+                    keepPlaying = true;
+                }
+                else
+                {
+                    keepPlaying = false;
+                }
+
+                
             }
+             
         }
     }   
 }
